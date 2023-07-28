1415dcde531c227d543d362061b07d8823afc639
VENTA5 <140033786+VENTA5@users.noreply.github.com>
2023-07-28 11:09:22 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillsMember() {
+  var member = document.getElementById("member");
+  var skills = document.getElementById("skills");
+  var projects = document.getElementById("projects");
+  var contact = document.getElementById("contact");
+  var memberBox = document.getElementById("member-box");
+  var skillsBox = document.getElementById("skills-box");
+  var projectsBox = document.getElementById("projects-box");
+  var contactBox = document.getElementById("contact-box");
+  member.style.color = "#fff";
+  skills.style.color = "#000";
+  projects.style.color = "#000";
+  contact.style.color = "#000";
+  memberBox.style.display = "block";
+  skillsBox.style.display = "none";
+  projectsBox.style.display = "none";
+  contactBox.style.display = "none";
+}  
